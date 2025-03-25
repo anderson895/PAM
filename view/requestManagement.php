08345a70673dfc36952bafef023cbf82e2d473c8
JARP <andersonandy046@gmail.com>
2025-03-25 19:16:14 +0800

    update

--- view/requestManagement.php
+++ view/requestManagement.php
@@ -8,11 +8,11 @@
     <h2 class="text-lg font-semibold text-gray-700">Supply Request</h2>
     <div class="flex items-center space-x-3">
         <?php
-        $userImage = !empty($On_Session[0]['profile_image']) ? $On_Session[0]['profile_image'] : null;
+        $userImage = !empty($On_Session[0]['profile_picture']) ? $On_Session[0]['profile_picture'] : null;
         ?>
         <div class="w-10 h-10 rounded-full overflow-hidden flex items-center justify-center bg-gray-200 text-gray-600">
             <?php if ($userImage): ?>
-                <img src="<?php echo $userImage; ?>" alt="User Avatar" class="w-full h-full object-cover">
+                <img src="../uploads/images/<?php echo $userImage; ?>" alt="User Avatar" class="w-full h-full object-cover">
             <?php else: ?>
                 <span class="material-icons text-3xl">account_circle</span>
             <?php endif; ?>
--- view/users.php
+++ view/users.php
@@ -15,11 +15,13 @@ if($_SESSION['role']=="Administrator"){
     <h2 class="text-lg font-semibold text-gray-700">Manage Users</h2>
     <div class="flex items-center space-x-3">
         <?php
-        $userImage = !empty($On_Session[0]['profile_image']) ? $On_Session[0]['profile_image'] : null;
+        $userImage = !empty($On_Session[0]['profile_picture']) ? $On_Session[0]['profile_picture'] : null;
+
+      
         ?>
         <div class="w-10 h-10 rounded-full overflow-hidden flex items-center justify-center bg-gray-200 text-gray-600">
             <?php if ($userImage): ?>
-                <img src="<?php echo $userImage; ?>" alt="User Avatar" class="w-full h-full object-cover">
+                <img src="../uploads/images/<?php echo $userImage; ?>" alt="User Avatar" class="w-full h-full object-cover">
             <?php else: ?>
                 <span class="material-icons text-3xl">account_circle</span>
             <?php endif; ?>
